57c5b205a3b8573a2a2c3d917c47956677e34b87
Peter Vilja <peter.vilja@gmail.com>
2014-05-08 23:42:07 +0300

    show training results on training page

--- app/scripts/views/showTraining.js
+++ app/scripts/views/showTraining.js
@@ -20,6 +20,7 @@ module.exports = Backbone.View.extend({
   },
 
   render: function () {
+    console.log(this.model.attributes);
     this.$el.html(this.template(this.model.attributes));
     return this;
   },
